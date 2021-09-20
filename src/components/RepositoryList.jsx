61565a244287b60aaf6af6847e0e1aea41130bef
Eduardo Torres <eduutorres@hotmail.com>
2021-09-20 20:51:32 -0300

    Propriedades no React

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,22 @@
+import { RepositoryItem } from './RepositoryItem';
+
+const repositoryProps = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/',
+};
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <RepositoryItem repository={repositoryProps} />
+        <RepositoryItem repository={repositoryProps} />
+        <RepositoryItem repository={repositoryProps} />
+        <RepositoryItem repository={repositoryProps} />
+      </ul>
+    </section>
+  );
+}
